942e7b4a0526a5fba5b31923679c2ee883d321cf
Jose <sanjuanjojo@gmail.com>
2015-08-15 23:36:57 +0800

    Added Joli Template

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -34,17 +34,45 @@ var Login = React.createClass({
     },
     render: function() {
         return (
-            <div id="login">
-                <h1>Login</h1>
-                Username:
-                <input ref="username" type="text" />
-                Password:
-                <input ref="password" type="text" />
-                <button type="submit" onClick={this.login}>Login</button>
-                <button onClick={this.register}>Register</button>
-                {this.state.error && (
-                    <h5>Incorrect username or password</h5>
-                )}
+             <div class="login-container">
+            
+                <div class="login-box animated fadeInDown">
+                    <div class="login-logo"></div>
+                    <div class="login-body">
+                        <div class="login-title"><strong>Welcome</strong>, Please login</div>
+                        <form action="index.html" class="form-horizontal" method="post">
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <input type="text" class="form-control" placeholder="Username"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <input type="password" class="form-control" placeholder="Password"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <a href="#" class="btn btn-link btn-block">Forgot your password?</a>
+                            </div>
+                            <div class="col-md-6">
+                                <button class="btn btn-info btn-block">Log In</button>
+                            </div>
+                        </div>
+                        </form>
+                    </div>
+                    <div class="login-footer">
+                        <div class="pull-left">
+                            &copy; 2014 AppName
+                        </div>
+                        <div class="pull-right">
+                            <a href="#">About</a> |
+                            <a href="#">Privacy</a> |
+                            <a href="#">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         );
     }
