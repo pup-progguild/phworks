31e7aa29c45438d2a821e6195000e03f473d5717
Ben <iglesia.ben@gmail.com>
2015-08-21 16:30:51 +0800

    Update search ui.

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -3,26 +3,56 @@
 var React = require('react'),
     $ = require('jquery'),
     Auth = require('./Auth'),
-    NavBar = require('./NavBar');
+    NavBar = require('./NavBar'),
+    Navigation = require('react-router').Navigation,
+    ReactBootstrap = require('react-bootstrap'),
+    Row = ReactBootstrap.Row,
+    Col = ReactBootstrap.Col,
+    Thumbnail = ReactBootstrap.Thumbnail,
+    Input = ReactBootstrap.Input,
+    ButtonInput = ReactBootstrap.ButtonInput,
+    Button = ReactBootstrap.Button,
+    Well = ReactBootstrap.Well,
+    Glyphicon = ReactBootstrap.Glyphicon;
 
 var SearchResultItem = React.createClass({
+    getInitialState: function() {
+        var ip = 'http://128.199.227.235';
+
+        return ({
+            textUrl: ip + '/api/send'
+        });
+    },
+    mixins: [
+        Navigation
+    ],
+    goToProfile: function() {
+        this.transitionTo('/profile');
+    },
+    sendMessage: function() {
+        console.log('send message');
+    },
     render: function() {
         return (
-            <div>
-                <p>{this.props.id}</p>
-                <p>{this.props.name}</p>
-                <p>{this.props.city}</p>
-                <p>{this.props.prov}</p>
-                <p>{this.props.city}</p>
-                <p>{this.props.profile_summary}</p>
-                <p>{this.props.rating}</p>
-                <p>{this.props.tagPoints}</p>
-                <p>{this.props.tags}</p>
-                <p>{this.props.locPoints}</p>
-                <p>{this.props.number}</p>
-                <p>{this.props.email}</p>
-                <hr />
-            </div>
+            <Well bsSize="large">
+                <Row>
+                    <Col lg={2}>
+                        <Thumbnail src="https://avatars1.githubusercontent.com/u/9935276" alt="Profile Photo" />
+                        <Button onClick={this.sendMessage}><Glyphicon glyph="phone" /></Button>
+                        <Button onClick={this.goToProfile}><Glyphicon glyph="user" /></Button>
+                    </Col>
+                    <Col lg={5}>
+                        <h5>{this.props.name}</h5>
+                        <p><Glyphicon glyph="map-marker" />&nbsp;&nbsp;{this.props.city}, {this.props.prov}</p>
+                        <p>Rating: <Glyphicon glyph="star" /> {this.props.rating}</p>
+                    </Col>
+                    <Col lg={5}>
+                        <p>tagpoints: {this.props.tagPoints}</p>
+                        <p>tags: {this.props.tags}</p>
+                        <p>locpoints: {this.props.locPoints}</p>
+                    </Col>
+                </Row>
+            </Well>
         );
     }
 });
@@ -31,23 +61,21 @@ var SearchResult = React.createClass({
     render: function() {
         var results = this.props.results.map(function(result) {
             return <SearchResultItem
-                        id={result.user_id}
                         name={result.name}
                         city={result.city_name}
                         prov={result.provname}
-                        profile_summary={result.profile_summary}
                         rating={result.rating}
                         tagPoints={result.tagPoints}
                         tags={result.tags}
                         locPoints={result.locationPoints}
                         number={result.contact}
-                        email={result.email}
                    />;
         });
         console.log(results);
         return (
             <div>
-                <h1>Search Results: </h1>
+                <h2><Glyphicon glyph="book" />&nbsp;&nbsp;Search Result/s</h2>
+                <br />
                 {results}
             </div>
         );
@@ -104,7 +132,6 @@ var Search = React.createClass({
             userIdUrl: ip + '/api/authenticate/user?token=' + Auth.getToken(),
             fieldsUrl: ip + '/api/field',
             searchUrl: ip + '/api/request',
-            textUrl: ip + '/api/send',
             fields: [],
             searchToggle: false
         });
@@ -112,8 +139,8 @@ var Search = React.createClass({
     onSearch: function(e) {
         e.preventDefault();
         var sr = {
-            id: this.refs.field.getDOMNode().value,
-            desc: this.refs.desc.getDOMNode().value
+            id: this.refs.field.getValue(),
+            desc: this.refs.desc.getValue()
         };
 
         this.search(this.state.userId, sr.id, sr.desc, (function(res) {
@@ -135,17 +162,24 @@ var Search = React.createClass({
         return (
             <div id="search">
                 <NavBar />
-                <h1>Search</h1>
-                fields:
-                <select ref="field">
-                    {options}
-                </select>
-                description:
-                <input ref="desc" type="text" />
-                <button type="submit" onClick={this.onSearch}>search</button>
-                {this.state.searchToggle && (
-                    <SearchResult results={this.state.searchResult}/>
-                )}
+                <Row className="container">
+                    <Col lg={3} lgOffset={1}>
+                        <h2><Glyphicon glyph="search" />&nbsp;&nbsp;Search</h2>
+                        <br />
+                        <form>
+                            <Input ref="field" type="select" label="Field">
+                                {options}
+                            </Input>
+                            <Input ref="desc" type="textarea" label="Description" placeholder="Description"/>
+                            <ButtonInput type="submit" value="Search" onClick={this.onSearch} />
+                        </form>
+                    </Col>
+                    <Col lg={8}>
+                        {this.state.searchToggle && (
+                            <SearchResult results={this.state.searchResult}/>
+                        )}
+                    </Col>
+                </Row>
             </div>
         );
     }
